a8a40450a62bfbf3d749d5f8f47a6f060603aed8
Alex Berberi <alex.l.berberi@gmail.com>
2018-04-13 01:59:16 -0400

    actually remove react social login library

--- assets/js/src/components/AmazonButton.jsx
+++ assets/js/src/components/AmazonButton.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-// import SocialLogin from "react-social-login";
-
-// const LoginButton = ({ children, triggerLogin, ...props }) => (
-//   <button onClick={triggerLogin} {...props}>
-//     {children}
-//   </button>
-// );
-
-// export default SocialLogin(LoginButton);
 
 export default function AmazonButton({ onClick }) {
-  return (<button onClick={onClick}>
-      <img border="0" alt="Login with Amazon"
-           src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
-           width="156" height="32" />
-  </button>)
+  return (
+    <button onClick={onClick}>
+      <img
+        border="0"
+        alt="Login with Amazon"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
+        width="156"
+        height="32"
+      />
+    </button>
+  );
 }
--- assets/js/src/containers/Login/reducer.js
+++ assets/js/src/containers/Login/reducer.js
@@ -3,15 +3,14 @@ import { LOGIN } from "./actions";
 
 // placeholder state and actions for example purposes, to be filled in later
 const initialState = {
-    token: "not set"
+  token: "not set"
 };
 
 export default createReducer(initialState, {
-    [LOGIN]: state => {
-        console.log("login action fired");
-        const options = { scope : 'profile' };
-        amazon.Login.authorize(options,
-            'https://jeopardy.superkewl.xyz');
-        return state;
-    }
+  [LOGIN]: state => {
+    console.log("login action fired");
+    const options = { scope: "profile" };
+    amazon.Login.authorize(options, "https://jeopardy.superkewl.xyz");
+    return state;
+  }
 });
