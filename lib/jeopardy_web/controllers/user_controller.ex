450762c43a56cfaba2b09913c707ee082be3afd8
Andrew Singley <singley.a@husky.neu.edu>
2018-04-20 00:36:11 -0400

    Added AMAZON user profile func

--- lib/jeopardy_web/controllers/user_controller.ex
+++ lib/jeopardy_web/controllers/user_controller.ex
@@ -11,6 +11,41 @@ defmodule JeopardyWeb.UserController do
     render(conn, "index.json", users: users)
   end
 
+  def get_profile(conn, %{"token" => token, "user_id" => user_id}) do
+
+    IO.puts("token: ")
+    IO.puts(token)
+
+    url_1 = "https://api.amazon.com/auth/o2/tokeninfo?access_token=" <> token
+    %{ "aud" => aud } = Poison.decode!(HTTPoison.get(url_1, [], [ ssl: [{:versions, [:'tlsv1.2']}] ]).body)
+
+    if (aud ==  "amzn1.application-oa2-client.7c7d7da492884579abc147dc6039141a") do
+
+      url_2 = "https://api.amazon.com/user/profile"
+      headers = ["Authorization": "Bearer #{token}"]
+      profile = Poison.decode!(HTTPoison.get(url_2, headers, [ ssl: [{:versions, [:'tlsv1.2']}] ]).body)
+
+      IO.puts("user_id: ")
+      IO.puts(user_id)
+
+      %{ amazon_uid: uuid } = Users.get_user(user_id)
+      %{ user_id: amzn_usr_id } = profile
+
+      if uuid === amzn_usr_id do
+
+        #Return success
+        send_resp(conn, 200, %{profile: profile})
+
+      else
+        #Return failure
+        send_resp(conn, 200, %{profile: %{}})
+      end
+    else
+      #Return failure
+      send_resp(conn, 200, %{profile: %{}})
+    end
+  end
+
   def create(conn, %{"user" => user_params}) do
     with {:ok, %User{} = user} <- Users.create_user(user_params) do
       conn
